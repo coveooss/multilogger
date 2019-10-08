a5e17e05949003f5fab0ea220d67df782301a003
Julien Duchesne <jduchesne@coveo.com>
2019-10-08 13:25:31 -0400

    Add level parsing functions

--- main.go
+++ main.go
@@ -3,13 +3,64 @@ package multilogger
 import (
 	"fmt"
 	"io/ioutil"
+	"strconv"
+	"strings"
 
 	"github.com/sirupsen/logrus"
 	easy "github.com/t-tomalak/logrus-easy-formatter"
 )
 
 // DisabledLevel can be set when one of the logging hooks should be disabled
-const DisabledLevel = 9999
+const DisabledLevel logrus.Level = 9999
+
+// DisabledLevelName is the textual name of the
+const DisabledLevelName string = "disabled"
+
+func AcceptedLevels() []string {
+	levels := []string{DisabledLevelName}
+	for _, level := range logrus.AllLevels {
+		levels = append(levels, level.String())
+	}
+	return levels
+}
+
+func AcceptedLevelsString() string {
+	return strings.Join(AcceptedLevels(), ", ")
+}
+
+// ParseLogLevel converts a string or number into a logging level
+func ParseLogLevel(level interface{}) logrus.Level {
+	logLevelFromInt := func(levelNum int) logrus.Level {
+		if levelNum == int(DisabledLevel) {
+			return DisabledLevel
+		}
+		return logrus.Level(levelNum)
+	}
+
+	if logrusLevel, ok := level.(logrus.Level); ok {
+		return logrusLevel
+	}
+
+	if levelNum, ok := level.(int); ok {
+		return logLevelFromInt(levelNum)
+	}
+
+	if levelString, ok := level.(string); ok {
+		if levelNum, err := strconv.Atoi(levelString); err == nil {
+			return logLevelFromInt(levelNum)
+		}
+
+		if strings.ToLower(levelString) == DisabledLevelName {
+			return DisabledLevel
+		}
+		parsedLevel, err := logrus.ParseLevel(levelString)
+		if err != nil {
+			panic(fmt.Errorf("Unable to parse the given logging level %s: %v", level, err))
+		}
+		return parsedLevel
+	}
+	panic(fmt.Errorf("Unable to parse the given logging level %v. It has to be a string or an integer", level))
+}
 
 // MultiLogger represents a logger that logs to both a file and the console at different (configurable) levels
 type MultiLogger struct {
@@ -21,7 +72,7 @@ type MultiLogger struct {
 }
 
 // New creates a new Multilogger instance
-func New(consoleLevel logrus.Level, fileLevel logrus.Level, fileName string, module string) *MultiLogger {
+func New(consoleLevel interface{}, fileLevel interface{}, fileName string, module string) *MultiLogger {
 	logger := &MultiLogger{
 		Logger: logrus.New(),
 	}
@@ -35,22 +86,22 @@ func New(consoleLevel logrus.Level, fileLevel logrus.Level, fileName string, mod
 	if fileName == "" {
 		fileName = module + ".log"
 	}
-	logger.fileHook = NewFileHook(fileName, fileLevel, formatter)
-	logger.consoleHook = NewConsoleHook(consoleLevel, formatter)
+	logger.fileHook = NewFileHook(fileName, ParseLogLevel(fileLevel), formatter)
+	logger.consoleHook = NewConsoleHook(ParseLogLevel(consoleLevel), formatter)
 	logger.refreshLoggers()
 
 	return logger
 }
 
 // SetConsoleLevel modifies the logging level for the console logger
-func (logger *MultiLogger) SetConsoleLevel(level logrus.Level) {
-	logger.consoleHook.MinimumLevel = level
+func (logger *MultiLogger) SetConsoleLevel(level interface{}) {
+	logger.consoleHook.MinimumLevel = ParseLogLevel(level)
 	logger.refreshLoggers()
 }
 
 // SetFileLevel modifies the logging level for the file logger
-func (logger *MultiLogger) SetFileLevel(level logrus.Level) {
-	logger.fileHook.MinimumLevel = level
+func (logger *MultiLogger) SetFileLevel(level interface{}) {
+	logger.fileHook.MinimumLevel = ParseLogLevel(level)
 	logger.refreshLoggers()
 }
 
