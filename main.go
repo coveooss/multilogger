6444f27845a27b9b4548e5491753be037f802c8a
Jo Giroux <jgiroux@coveo.com>
2019-10-10 12:08:24 -0400

    Add error on New & ParseLogLevel
    Instead of panicking if a user supply an invalid value for logging level, we simply return an error and we provide a MustParseLogLevel that will panic on invalid value.

--- main.go
+++ main.go
@@ -31,8 +31,18 @@ func AcceptedLevelsString() string {
 	return strings.Join(AcceptedLevels(), ", ")
 }
 
+// MustParseLogLevel converts a string or number into a logging level
+// It panics if the supplied valid cannot be converted into a valid logrus Level
+func MustParseLogLevel(level interface{}) logrus.Level {
+	result, err := ParseLogLevel(level)
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+
 // ParseLogLevel converts a string or number into a logging level
-func ParseLogLevel(level interface{}) logrus.Level {
+func ParseLogLevel(level interface{}) (logrus.Level, error) {
 	logLevelFromInt := func(levelNum int) logrus.Level {
 		if levelNum == int(DisabledLevel) {
 			return DisabledLevel
@@ -41,28 +51,28 @@ func ParseLogLevel(level interface{}) logrus.Level {
 	}
 
 	if logrusLevel, ok := level.(logrus.Level); ok {
-		return logrusLevel
+		return logrusLevel, nil
 	}
 
 	if levelNum, ok := level.(int); ok {
-		return logLevelFromInt(levelNum)
+		return logLevelFromInt(levelNum), nil
 	}
 
 	if levelString, ok := level.(string); ok {
 		if levelNum, err := strconv.Atoi(levelString); err == nil {
-			return logLevelFromInt(levelNum)
+			return logLevelFromInt(levelNum), nil
 		}
 
 		if strings.ToLower(levelString) == DisabledLevelName {
-			return DisabledLevel
+			return DisabledLevel, nil
 		}
 		parsedLevel, err := logrus.ParseLevel(levelString)
 		if err != nil {
-			panic(fmt.Errorf("Unable to parse the given logging level %s: %v", level, err))
+			return 0, fmt.Errorf("Unable to parse logging level: %v", err)
 		}
-		return parsedLevel
+		return parsedLevel, nil
 	}
-	panic(fmt.Errorf("Unable to parse the given logging level %v. It has to be a string or an integer", level))
+	return 0, fmt.Errorf("Unable to parse the given logging level %v. It has to be a string or an integer", level)
 }
 
 // MultiLogger represents a logger that logs to both a file and the console at different (configurable) levels
@@ -74,45 +84,58 @@ type MultiLogger struct {
 	fileHook    *FileHook
 }
 
-// New creates a new Multilogger instance
+// New creates a new Multilogger instance and panic if it can't be created
 func New(consoleLevel interface{}, fileLevel interface{}, filename string, module string) *MultiLogger {
-	logger := &MultiLogger{
-		Logger: logrus.New(),
+	logger, err := Create(consoleLevel, fileLevel, filename, module)
+	if err != nil {
+		panic(err)
 	}
+	return logger
+}
+
+// Create creates a new Multilogger instance
+func Create(consoleLevel interface{}, fileLevel interface{}, filename string, module string) (logger *MultiLogger, err error) {
+	if consoleLevel, err = ParseLogLevel(consoleLevel); err != nil {
+		return
+	}
+	if fileLevel, err = ParseLogLevel(fileLevel); err != nil {
+		return
+	}
+	logger = &MultiLogger{Logger: logrus.New()}
 	logger.Out = ioutil.Discard      // Discard all logs to the main logger
 	logger.Level = logrus.TraceLevel // Always log at TRACE level. Hooks will decide if the log goes through or not
 
 	format := fmt.Sprintf("[%s]", module) + " %time% %lvl% %msg%\n"
-	logger.fileHook = NewFileHook(filename, ParseLogLevel(fileLevel), &Formatter{
+	logger.fileHook = NewFileHook(filename, fileLevel.(logrus.Level), &Formatter{
 		TimestampFormat: timestampFormat,
 		LogFormat:       format,
 		Color:           false,
 	})
-	logger.consoleHook = NewConsoleHook(ParseLogLevel(consoleLevel), &Formatter{
+	logger.consoleHook = NewConsoleHook(consoleLevel.(logrus.Level), &Formatter{
 		TimestampFormat: timestampFormat,
 		LogFormat:       format,
 		Color:           true,
 	})
 	logger.refreshLoggers()
 
-	return logger
+	return
 }
 
 // SetConsoleLevel modifies the logging level for the console logger
 func (logger *MultiLogger) SetConsoleLevel(level interface{}) {
-	logger.consoleHook.MinimumLevel = ParseLogLevel(level)
+	logger.consoleHook.MinimumLevel = MustParseLogLevel(level)
 	logger.refreshLoggers()
 }
 
 // SetFileLevel modifies the logging level for the file logger
 func (logger *MultiLogger) SetFileLevel(level interface{}) {
-	logger.fileHook.MinimumLevel = ParseLogLevel(level)
+	logger.fileHook.MinimumLevel = MustParseLogLevel(level)
 	logger.refreshLoggers()
 }
 
 // ConfigureFileLogger modifies the logging level and filename for the file logger
 func (logger *MultiLogger) ConfigureFileLogger(level interface{}, newFilename string) {
-	logger.fileHook.MinimumLevel = ParseLogLevel(level)
+	logger.fileHook.MinimumLevel = MustParseLogLevel(level)
 	logger.fileHook.SetFilename(newFilename)
 	logger.refreshLoggers()
 }
--- main_test.go
+++ main_test.go
@@ -2,6 +2,7 @@ package multilogger
 
 import (
 	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/sirupsen/logrus"
@@ -9,18 +10,31 @@ import (
 )
 
 func TestParseLevel(t *testing.T) {
-	assert.Equal(t, DisabledLevel, ParseLogLevel(DisabledLevel))
-	assert.Equal(t, DisabledLevel, ParseLogLevel(DisabledLevelName))
-	assert.Equal(t, DisabledLevel, ParseLogLevel(int(DisabledLevel)))
-	assert.Equal(t, DisabledLevel, ParseLogLevel(strconv.Itoa(int(DisabledLevel))))
+	assert.Equal(t, DisabledLevel, MustParseLogLevel(DisabledLevel))
+	assert.Equal(t, DisabledLevel, MustParseLogLevel(DisabledLevelName))
+	assert.Equal(t, DisabledLevel, MustParseLogLevel(int(DisabledLevel)))
+	assert.Equal(t, DisabledLevel, MustParseLogLevel(strconv.Itoa(int(DisabledLevel))))
 	for _, level := range logrus.AllLevels {
-		assert.Equal(t, level, ParseLogLevel(level))
-		assert.Equal(t, level, ParseLogLevel(level.String()))
-		assert.Equal(t, level, ParseLogLevel(int(level)))
-		assert.Equal(t, level, ParseLogLevel(strconv.Itoa(int(level))))
+		name := level.String()
+		assert.Equal(t, level, MustParseLogLevel(level))
+		assert.Equal(t, level, MustParseLogLevel(name))
+		assert.Equal(t, level, MustParseLogLevel(strings.ToUpper(name)))
+		assert.Equal(t, level, MustParseLogLevel(strings.ToLower(name)))
+		assert.Equal(t, level, MustParseLogLevel(strings.ToTitle(name)))
+		assert.Equal(t, level, MustParseLogLevel(int(level)))
+		assert.Equal(t, level, MustParseLogLevel(strconv.Itoa(int(level))))
 	}
 }
 
 func TestGetAcceptedLevels(t *testing.T) {
 	assert.Equal(t, "disabled, panic, fatal, error, warning, info, debug, trace", AcceptedLevelsString())
+}
+
+func TestParseInvalidLogLevel(t *testing.T) {
+	level, err := ParseLogLevel("invalid")
+	assert.Equal(t, level, logrus.Level(0))
+	assert.EqualError(t, err, `Unable to parse logging level: not a valid logrus Level: "invalid"`)
+	level, err = ParseLogLevel(1.234)
+	assert.Equal(t, level, logrus.Level(0))
+	assert.EqualError(t, err, `Unable to parse the given logging level 1.234. It has to be a string or an integer`)
 }
