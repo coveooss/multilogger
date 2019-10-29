9638b0f7147128f6797be86e4c15f4f6f649e788
Jo Giroux <jgiroux@coveo.com>
2019-10-29 13:54:35 -0400

    Split New example into multiple examples

--- color/color_test.go
+++ color/color_test.go
@@ -9,31 +9,51 @@ import (
 )
 
 func ExampleNew() {
-	// New is very flexible.
-
 	// It accepts an enumeration of attributes.
-	New(color.FgGreen, color.Underline)
+	writer := New(color.FgGreen, color.Underline)
+	fmt.Printf("%+v", *writer.Color)
+	// Output:
+	// {params:[32 4] noColor:<nil>}
+}
 
-	// Or an array of attributes.
-	var attributes []color.Attribute = Attributes(color.FgGreen, color.Underline)
-	New(attributes)
+func ExampleNew_with_array() {
+	// It also accepts an array of attributes.
+	writer := New([]color.Attribute{color.FgGreen, color.Underline})
+	fmt.Printf("%+v", *writer.Color)
+	// Output:
+	// {params:[32 4] noColor:<nil>}
+}
 
-	// As well as an enumeration of strings.
-	New("Green", "Underline")
+func ExampleNew_with_strings() {
+	// It also accepts a list of string attributes.
+	writer := New("FgHiGreen", "Underline", "BgYellow")
+	fmt.Printf("%+v", *writer.Color)
+	// Output:
+	// {params:[92 4 43] noColor:<nil>}
+}
 
+func ExampleNew_mixup() {
 	// It can also mix strings and attributes
-	New(color.BgRed, "crossedout")
+	writer := New(color.BgRed, "crossedout")
+	fmt.Printf("%+v", *writer.Color)
+	// Output:
+	// {params:[41 9] noColor:<nil>}
+}
 
-	// Attributes can also be separated by spaces.
-	New("Green Underline")
-
+func ExampleNew_string_separated() {
 	// Or separated by any non letter. Case is also non significant.
-	New("RED | underline, CrossedOUT+BgYellow")
+	writer := New("RED | underline, CrossedOUT+BgYellow")
+	fmt.Printf("%+v", *writer.Color)
+	// Output:
+	// {params:[31 4 9 43] noColor:<nil>}
+}
 
+func ExampleNew_error() {
 	// But it panics if you supplied invalid attribute.
 	func() {
 		defer func() { fmt.Println(recover()) }()
-		New("red, invalid | rouge")
+		color := New("red, invalid | rouge")
+		fmt.Printf("%+v", color)
 	}()
 	// Output:
 	// Attribute not found invalid
